269a4187cda276bba34e80db05b21312bb96d10b
Adrianh De Lucio Chavero <adrianh_coder@outlook.com>
2025-07-03 12:30:54 -0600

    Add account_move to manifest and model imports for sale order prompt extractor

--- __manifest__.py
+++ __manifest__.py
@@ -14,6 +14,7 @@
     'data': [
         'views/res_config_settings_views.xml',
         'views/sale_order_view.xml',
+        'views/account_move_actions.xml',
     ],
     'installable': True,
     'application': False,
